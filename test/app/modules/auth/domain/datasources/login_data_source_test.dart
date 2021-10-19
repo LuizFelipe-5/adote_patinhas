b51e36f697bcccf6308e466ebc74a0bdb4940874
Luiz Montuani <luizmontuani@ioasys.com.br>
2021-10-19 12:58:01 -0300

    :bug: fix login_data_source_test

--- test/app/modules/auth/domain/datasources/login_data_source_test.dart
+++ test/app/modules/auth/domain/datasources/login_data_source_test.dart
@@ -1,3 +1,4 @@
+import 'package:adote_patinhas/app/modules/auth/data/datasources/login_data_source_impl.dart';
 import 'package:adote_patinhas/app/modules/auth/domain/datasources/login_data_source.dart';
 import 'package:adote_patinhas/app/modules/auth/domain/repositories/login_repository.dart';
 import 'package:dartz/dartz.dart';
@@ -5,21 +6,19 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class LoginDataSourceMock extends Mock implements LoginDataSource {}
-
 class FirebaseAuthMock extends Mock implements FirebaseAuth {}
 
 class UserCredentialMock extends Mock implements UserCredential {}
 
 void main() {
-  late LoginDataSourceMock dataSource;
+  late LoginDataSource dataSource;
   late FirebaseAuthMock firebaseAuth;
   late UserCredential userCredential;
 
   setUp(() {
     userCredential = UserCredentialMock();
-    dataSource = LoginDataSourceMock();
     firebaseAuth = FirebaseAuthMock();
+    dataSource = LoginDataSourceImpl(firebaseAuth: firebaseAuth);
   });
 
   test('Deveria retornar um obj do tipo Right', () async {
