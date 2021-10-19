8b1684539ec045d9bc68f6e87990db15d59b41ca
Luiz Montuani <luizmontuani@ioasys.com.br>
2021-10-19 16:17:03 -0300

    :sparkles: firebase failures

---
+++ lib/app/modules/auth/domain/failures/email_already_in_use_failure.dart
@@ -0,0 +1,3 @@
+import 'package:adote_patinhas/app/core/failure.dart';
+
+class EmailAlreadyInUseFailure implements Failure {}
---
+++ lib/app/modules/auth/domain/failures/weak_password_failure.dart
@@ -0,0 +1,3 @@
+import 'package:adote_patinhas/app/core/failure.dart';
+
+class WeakPasswordFailure implements Failure {}
