3bbeae327c0b7beea9d4583ce3a7501d1c11d793
Luiz Montuani <luizmontuani@ioasys.com.br>
2021-10-26 16:40:57 -0300

    :hammer: add string number to user_entity

--- lib/app/modules/profile/domain/entities/user_entity.dart
+++ lib/app/modules/profile/domain/entities/user_entity.dart
@@ -3,9 +3,11 @@ abstract class UserEntity {
   String cep;
   String city;
   String state;
+  String number;
 
   UserEntity({
     required this.name,
+    required this.number,
     required this.cep,
     required this.city,
     required this.state,
